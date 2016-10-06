849628da9cdb243ba8525c3422548154a04e039c
Guilhem Lettron <g.lettron@criteo.com>
2016-10-06 18:38:38 +0200

    Release 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem@lettron.fr'
 license 'apachev2'
 description 'Installs/Configures traefik'
 long_description 'Installs/Configures traefik'
-version '0.1.0'
+version '0.1.2'
 
 issues_url 'https://github.com/redguide/traefik/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/redguide/traefik' if respond_to?(:source_url)
