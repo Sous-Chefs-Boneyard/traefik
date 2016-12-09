d28089a43f1fbf830f26bf5f3d9680e0c18a781e
Guilhem Lettron <g.lettron@criteo.com>
2016-12-09 16:06:01 +0100

    Update traefik to 1.1.1 and remove checksum
    You should add your own checksum

--- attributes/install.rb
+++ attributes/install.rb
@@ -1,7 +1,7 @@
-default['traefik']['install']['version'] = '1.0.3'
+default['traefik']['install']['version'] = '1.1.1'
 default['traefik']['install']['install_method'] = 'binary'
 
 default['traefik']['install']['binary']['version'] = node['traefik']['install']['version']
 default['traefik']['install']['binary']['url'] = "https://github.com/containous/traefik/releases/download/v#{node['traefik']['install']['binary']['version']}/traefik_linux-amd64"
-default['traefik']['install']['binary']['checksum'] = '784ede42d839c6e40781f59ddb63f20332be083d2e1f9bbd9593cf4f35a7cc0e'
+default['traefik']['install']['binary']['checksum'] = nil
 default['traefik']['install']['binary']['path'] = '/usr/local/bin/traefik'
