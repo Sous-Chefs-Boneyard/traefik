ade77f3e3b6c982034fcfb2fd60eeb6c2abde418
Guilhem Lettron <g.lettron@criteo.com>
2017-04-25 19:25:48 +0200

    Fix foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,18 @@
 name 'traefik'
-maintainer 'The Authors'
+maintainer 'redguide team'
 maintainer_email 'guilhem@lettron.fr'
-license 'apachev2'
+license 'Apache-2.0'
 description 'Installs/Configures traefik'
 long_description 'Installs/Configures traefik'
 version '0.1.3'
 
+chef_version '>= 12.8' if respond_to?(:chef_version)
+
 issues_url 'https://github.com/redguide/traefik/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/redguide/traefik' if respond_to?(:source_url)
+
+supports 'centos'
+supports 'ubuntu'
 
 depends 'systemd'
 
